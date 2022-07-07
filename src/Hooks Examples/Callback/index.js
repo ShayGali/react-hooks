7fe1113f1493839d081ebe1145f866c77a97f115
ShayGali <shayali100@gmail.com>
2022-07-07 19:42:08 +0300

    add table in README.md

--- src/Hooks Examples/Callback/index.js
+++ src/Hooks Examples/Callback/index.js
@@ -1,5 +1,10 @@
 import React from "react";
+import First from "./components/First";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <First />
+    </>
+  );
 }
